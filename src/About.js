6f6c35ffcaccf9e4cb0a2bdc75a844c812b87d86
Amith Chauhan <amithchauhan2000@gmail.com>
2022-03-24 10:21:58 +0530

    added content in About page

--- src/About.js
+++ src/About.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+    <h1>About</h1>
+    <pre>{
+    `      DU has a lot of student societies that conduct a lot of events every year,
+      but most of them face difficulties while getting the event sponsored to
+      make it more awesome and engage more students. We aim to solve this issue
+      by creating a centralised platform where these societies can either raise
+      funds or find sponsors who are willing to sponsor their awesome events.`
+    }
+    </pre>
+    </>
+  );
+};
 
-export default About
+export default About;
